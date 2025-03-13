3cd4d952b2cbf4f980c68b1deedde779e02e5c0a
MKVISHWAAS <141738245+MKVISHWAAS@users.noreply.github.com>
2025-03-13 19:35:43 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I (PES1UG22CS336) have successfully built and run this  to check the working of jenkins :) \n";
+  cout << "I (PES1UG22CS336)         have successfully built and run this  to check the working of jenkins :) \n";
   return 0;
 }
